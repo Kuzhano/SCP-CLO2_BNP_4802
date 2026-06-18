6a613af1a8a44736e3f2ddc1c64a352a51f5584a
ShykiFr <moh.dzaki.ayatillah@gmail.com>
2026-06-19 04:07:26 +0700

    Menambahkan file ProposalReview, ReviewController, ReviewStateMachine

---
+++ ReviewProposal/ReviewProposal/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ModulReviewPenilaian
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pastikan Anda sudah punya file proposals.json dari Modul 2 
+            // di directory bin/Debug/netcoreappX.X tempat file exe berjalan.
+            ReviewController reviewModule = new ReviewController();
+            reviewModule.TampilkanMenuReview();
+        }
+    }
+}
---
+++ ReviewProposal/ReviewProposal/ReviewStateMachine.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ModulReviewPenilaian
+{
+    public enum StatusProposal { PENDING, DITERIMA, DITOLAK, REVISI }
+    public enum AksiReview { TERIMA, TOLAK, MINTA_REVISI, KEMBALIKAN_PENDING }
+
+    public class ReviewStateMachine
+    {
+        // TK: Table-driven Construction
+        // Tabel transisi Automata yang memetakan (CurrentState, Action) -> NextState
+        private readonly Dictionary<(StatusProposal, AksiReview), StatusProposal> _transitionTable;
+
+        public ReviewStateMachine()
+        {
+            _transitionTable = new Dictionary<(StatusProposal, AksiReview), StatusProposal>
+            {
+                { (StatusProposal.PENDING, AksiReview.TERIMA), StatusProposal.DITERIMA },
+                { (StatusProposal.PENDING, AksiReview.TOLAK), StatusProposal.DITOLAK },
+                { (StatusProposal.PENDING, AksiReview.MINTA_REVISI), StatusProposal.REVISI },
+
+                { (StatusProposal.REVISI, AksiReview.TERIMA), StatusProposal.DITERIMA },
+                { (StatusProposal.REVISI, AksiReview.TOLAK), StatusProposal.DITOLAK },
+
+                { (StatusProposal.DITOLAK, AksiReview.KEMBALIKAN_PENDING), StatusProposal.PENDING }
+            };
+        }
+
+        // TK: Automata
+        // Method untuk mengeksekusi transisi state
+        public StatusProposal GetNextState(StatusProposal currentState, AksiReview action)
+        {
+            var key = (currentState, action);
+            if (_transitionTable.TryGetValue(key, out StatusProposal nextState))
+            {
+                return nextState;
+            }
+            throw new InvalidOperationException($"Transisi tidak valid: {currentState} tidak bisa melakukan aksi {action}.");
+        }
+
+        public static StatusProposal ParseStatus(string statusString)
+        {
+            if (Enum.TryParse(statusString, true, out StatusProposal status))
+                return status;
+            return StatusProposal.PENDING; // Default
+        }
+    }
+}
