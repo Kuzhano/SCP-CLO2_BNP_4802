450dda0d5c7df64f64e241b7e451e7875a329e2c
JustAHermit <arsall1202@gmail.com>
2026-06-18 23:47:27 +0700

    Add files via upload

---
+++ SCP-CLO2_BNP_4802/SCP-CLO2_BNP_4802/AppConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace SCP_CLO2_BNP_4802
+{
+    public class AppConfig
+    {
+        public string ProposalFilePath { get; set; } = "proposals.json";
+
+        public static AppConfig LoadConfiguration()
+        {
+            string configPath = "appconfig.json";
+            if (File.Exists(configPath))
+            {
+                try
+                {
+                    string jsonString = File.ReadAllText(configPath);
+                    return JsonSerializer.Deserialize<AppConfig>(jsonString) ?? new AppConfig();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Gagal membaca appconfig.json, menggunakan konfigurasi default.");
+                }
+            }
+            return new AppConfig();
+        }
+    }
+}
