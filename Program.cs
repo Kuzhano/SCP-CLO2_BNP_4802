687c2b6a2d68897d52254b8049bdb99d660eb719
ShykiFr <moh.dzaki.ayatillah@gmail.com>
2026-06-19 04:23:44 +0700

    Memperbaiki commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ModulReviewPenilaian
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReviewController reviewModule = new ReviewController();
+            reviewModule.TampilkanMenuReview();
+        }
+    }
+}
